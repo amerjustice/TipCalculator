9c5dfb9b8d3a34fdc448ca8bdd19cdb1fe7ba46d
amerjustice <amerjustice@outlook.com>
2016-05-13 14:27:48 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Susan Justice on 2016-05-13.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
