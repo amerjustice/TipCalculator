a6ea6ec3dd714725143f1d85cf3d9fa50be10d13
amerjustice <amerjustice@outlook.com>
2016-05-14 18:13:10 -0700

    Tip Calculator

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmount;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (weak, nonatomic) IBOutlet UISlider *tipPercentageSlider;
 
 @end
 
@@ -16,12 +20,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tipPercentageSlider.minimumValue = 0.0;
+    self.tipPercentageSlider.maximumValue = 100;
+    [self.tipPercentageSlider setValue:15.0 animated:YES];
+    self.tipPercentageTextField.text = [NSString stringWithFormat:@"%1.2f %%", self.tipPercentageSlider.value];
+    
+    
+    
 }
+- (IBAction)calculateTip:(id)sender {
+    
+    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+    f.numberStyle = NSNumberFormatterDecimalStyle;
+    NSNumber *billAmountNumber = [f numberFromString:self.billAmountTextField.text];
+    
+    float tip = self.tipPercentageSlider.value/100.0 * billAmountNumber.floatValue;
+    
+    self.tipAmount.text = [NSString stringWithFormat:@"%1.2f", tip]; 
+    
+
+    
+    
+    
+    
+    
+}
+- (IBAction)tipSlider:(id)sender {
+
+    self.tipPercentageTextField.text = [NSString stringWithFormat:@"%1.2f %%", self.tipPercentageSlider.value];
+}
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  
 }
 
 @end
